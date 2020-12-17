ea6ec3f78deec5d5314f007ec80f83233c8fb275
reuzun <moruka12@yandex.com>
2020-12-17 13:20:34 +0300

    code review.

--- src/main/java/ceng/estu/controller/MusicPlayerController.java
+++ src/main/java/ceng/estu/controller/MusicPlayerController.java
@@ -1,40 +1,28 @@
 package ceng.estu.controller;
 
-import com.jfoenix.controls.*;
-import de.jensd.fx.glyphs.GlyphIconName;
+
+import com.jfoenix.controls.JFXSlider;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.media.AudioTrack;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.Set;
 import java.util.Stack;
 
 public class MusicPlayerController implements Initializable {
@@ -46,7 +34,7 @@ public class MusicPlayerController implements Initializable {
     private double yOffset = 0;*/
 
     @FXML
-    private ListView lv;
+    private ListView<String> lv;
     @FXML
     private MediaView img;
     @FXML
@@ -54,7 +42,7 @@ public class MusicPlayerController implements Initializable {
     @FXML
     private Text remainingTime;
     @FXML
-    private Slider playingTimeSlider = new JFXSlider();
+    private JFXSlider playingTimeSlider = new JFXSlider();
     @FXML
     private Slider volumeSlider = new Slider();
     @FXML
@@ -62,26 +50,16 @@ public class MusicPlayerController implements Initializable {
     @FXML
     private FontAwesomeIcon playButton;
 
-    private Stack<String> playedSongs = new Stack<>();
-    @FXML
-    private AnchorPane mainPanel;
-    @FXML
-    private StackPane stackinthepanel;
+    private final Stack<String> playedSongs = new Stack<>();
     @FXML
     private ImageView imgView;
-    @FXML
-    private AnchorPane musicpanel;
-    @FXML
-    private JFXButton closeButton;
-    @FXML
-    private StackPane imgStackPane;
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
             adjustSongRepository();
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
         img.toFront();
     }
@@ -98,6 +76,11 @@ public class MusicPlayerController implements Initializable {
 
     @javafx.fxml.FXML
     public void startMusic(ActionEvent actionEvent) {
+        if(lv.getSelectionModel().getSelectedItem()==null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR,"Select a song please" , ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
 
         if (player.getStatus() == MediaPlayer.Status.PLAYING) {
             MainPanelController.staticLogo.setTextFill((Color.color(0, 0, 0, 1)));
@@ -129,7 +112,7 @@ public class MusicPlayerController implements Initializable {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < name.toCharArray().length; i++) {
             if (name.charAt(i) == '\\' && name.charAt(i + 1) != '\\') sb.append("\\\\");
-            else sb.append(String.valueOf((char) name.charAt(i)));
+            else sb.append(name.charAt(i));
         }
         name = sb.toString();
         return name;
@@ -142,224 +125,53 @@ public class MusicPlayerController implements Initializable {
     }
 
 
-    static MediaPlayer player;
+    private MediaPlayer player;
     static boolean voiceFlag = true;
 
     @FXML
-    public void startApplication(MouseEvent event) throws MalformedURLException {
-
+    public void startApplication(MouseEvent event) {
         if (event.getClickCount() == 2) {
             MainPanelController.staticLogo.setTextFill(Color.valueOf("#0f4ddb"));
             String str2 = (String) lv.getSelectionModel().getSelectedItem();
-            if (str2 == null) return; //if selects null then get out of method.
-            File musicFile = new File("musics" + File.separator + str2);
-            Media music;
-            try {
-                 music = new Media(musicFile.toURI().toURL().toString());
-            } catch (Exception exception) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Unsupported file format."
-                        , ButtonType.OK);
-                alert.setTitle("Error.");
-                alert.showAndWait();
-                return;
-            }
-
-            nameOfSong.setText(String.valueOf(lv.getSelectionModel().getSelectedItem()));
-
-            if (null != player && player.getStatus() == MediaPlayer.Status.PLAYING)
-                player.pause();
-
-            try {
-                player = new MediaPlayer(music);
-            } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Please run this command on terminal \n" +
-                        "sudo apt-get install ubuntu-restricted-extras", ButtonType.OK);
-                alert.setTitle("Your System is unable to run mp3/mp4.");
-                alert.showAndWait();
-                return;
-            }
-
-            playButton.setIconName("PAUSE");
-            if (voiceFlag) {
-                player.setVolume(0.25);
-                voiceFlag = false;
-            } else {
-                player.setVolume(volumeSlider.getValue());
-            }
-            player.setOnReady(() -> {
-                player.setStartTime(Duration.ZERO);
-
-                if (player.getMedia().getSource().contains(".mp3") || player.getMedia().getSource().contains(".wav")) {
-                    imgView.toFront();
-                } else {
-                    img.setMediaPlayer(player);
-                    img.toFront();
-                }
-
-                //VOLUME PROPERTİES
-                volumeSlider.setMin(0);
-                volumeSlider.setMax(0.5);
-                volumeSlider.setValue(player.getVolume());
-                VolumeText.setText(String.valueOf((int) (volumeSlider.getValue() * 100 * 2)));
-
-                volumeSlider.valueProperty().addListener(evs -> {
-                    player.setVolume(volumeSlider.getValue());
-                    VolumeText.setText(String.valueOf((int) (volumeSlider.getValue() * 100 * 2)));
-
-                });
-
-                playingTimeSlider.setMax(player.getStopTime().toMillis());
-                player.currentTimeProperty().addListener(ev -> {
-                    remainingTime.setText(String.valueOf((int) (player.getStopTime().toSeconds() - player.getCurrentTime().toSeconds())));
-                    playingTimeSlider.valueProperty().setValue(player.getCurrentTime().toMillis());
-                    if (!playingTimeSlider.isValueChanging()) {
-                        playingTimeSlider.setValue(player.getCurrentTime().toMillis());
-                    }
-                });
-
-                player.play();
-                playedSongs.push(player.getMedia().getSource().substring(player.getMedia().getSource().lastIndexOf("/") + 1));
-                //System.out.println(playedSongs.peek());
-                //StaticController.Amblem.setFill(Color.rgb(172,60,60,1));
-                playingTimeSlider.valueProperty().addListener(ov -> {
-                    if (playingTimeSlider.isValueChanging()) {
-                        player.seek(Duration.millis(new Duration(playingTimeSlider.getValue()).toMillis()));
-                    }
-
-                });
-
-                player.setOnEndOfMedia(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            playRandomMusic();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            });
-
-
+            playSong(str2);
         }
-
     }
 
-    public void playLastSong() throws MalformedURLException {
+    public void playLastSong() {
         MainPanelController.staticLogo.setTextFill(Color.valueOf("#0f4ddb"));
         String str = "";
         try {
             str = playedSongs.pop();
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
-        //int random = (int) (Math.random() * lv.getItems().size());
+
         if (playedSongs.size() == 0) {
             lv.getSelectionModel().select(str);
         } else
             lv.getSelectionModel().select(playedSongs.pop());
-
-
-        File musicFile = new File("musics" + File.separator + lv.getSelectionModel().getSelectedItem());
-
-
-        nameOfSong.setText(String.valueOf(lv.getSelectionModel().getSelectedItem()));//setting up text
-        Media music;
-        try {
-            music = new Media(musicFile.toURI().toURL().toString());
-        } catch (Exception exception) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Unsupported file format."
-                    , ButtonType.OK);
-            alert.setTitle("Error.");
-            alert.showAndWait();
-            return;
-        }
-
-        if (null != player && player.getStatus() == MediaPlayer.Status.PLAYING)
-            player.pause();
-
-        try {
-            player = new MediaPlayer(music);
-        } catch (Exception e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Please run this command on terminal \n" +
-                    "sudo apt-get install ubuntu-restricted-extras", ButtonType.OK);
-            alert.setTitle("Your System is unable to run mp3/mp4.");
-            alert.showAndWait();
-            return;
-        }
-        player.setVolume(volumeSlider.getValue());
-        player.setOnReady(new Runnable() {
-            @Override
-            public void run() {
-                player.setStartTime(Duration.ZERO);
-                img.setMediaPlayer(player);
-
-                if (player.getMedia().getSource().contains(".mp3") || player.getMedia().getSource().contains(".wav")) {
-                    imgView.toFront();
-                } else {
-                    img.setMediaPlayer(player);
-                    img.toFront();
-                }
-
-
-                //VOLUME PROPERTİES
-                volumeSlider.setMin(0);
-                volumeSlider.setMax(0.5);
-
-                VolumeText.setText(String.valueOf((int) (player.getVolume() * 100 * 2)));
-
-                volumeSlider.valueProperty().addListener(evs -> {
-                    player.setVolume(volumeSlider.getValue());
-                    VolumeText.setText(String.valueOf((int) (volumeSlider.getValue() * 100 * 2)));
-
-                });
-
-                playingTimeSlider.setMax(player.getStopTime().toMillis());
-                player.currentTimeProperty().addListener(ev -> {
-                    remainingTime.setText(String.valueOf((int) (player.getStopTime().toSeconds() - player.getCurrentTime().toSeconds())));
-                    playingTimeSlider.valueProperty().setValue(player.getCurrentTime().toMillis());
-                    if (!playingTimeSlider.isValueChanging()) {
-                        playingTimeSlider.setValue(player.getCurrentTime().toMillis());
-                    }
-                });
-
-                playingTimeSlider.valueProperty().addListener(ov -> {
-                    if (playingTimeSlider.isValueChanging()) {
-                        player.seek(Duration.millis(new Duration(playingTimeSlider.getValue()).toMillis()));
-                    }
-
-                });
-                player.play();
-                playedSongs.push(player.getMedia().getSource().substring(player.getMedia().getSource().lastIndexOf("/") + 1));
-
-                player.setOnEndOfMedia(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            playRandomMusic();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
-
+        playSong(lv.getSelectionModel().getSelectedItem());
     }
 
-    private void playRandomMusic() throws MalformedURLException {
+    private void playRandomMusic() {
         MainPanelController.staticLogo.setTextFill(Color.valueOf("#0f4ddb"));
         int random = (int) (Math.random() * lv.getItems().size());
         lv.getSelectionModel().select(random);
 
-        if (lv.getSelectionModel().getSelectedItem().toString().equals(playedSongs.peek())) {
-            playRandomMusic();
-            return;
+        try {
+            if (lv.getSelectionModel().getSelectedItem().equals(playedSongs.peek())) {
+                playRandomMusic();
+                return;
+            }
+        }catch (Exception ignored){
         }
+        playSong(lv.getSelectionModel().getSelectedItem());
 
-        File musicFile = new File("musics" + File.separator + lv.getSelectionModel().getSelectedItem());
+    }
 
+    private void playSong(String str){
 
-        nameOfSong.setText(String.valueOf(lv.getSelectionModel().getSelectedItem()));//setting up text
+        if (str == null) return; //if selects null then get out of method.
+        File musicFile = new File("musics" + File.separator + str);
         Media music;
         try {
             music = new Media(musicFile.toURI().toURL().toString());
@@ -371,8 +183,11 @@ public class MusicPlayerController implements Initializable {
             return;
         }
 
+        nameOfSong.setText(String.valueOf(lv.getSelectionModel().getSelectedItem()));
+
         if (null != player && player.getStatus() == MediaPlayer.Status.PLAYING)
-            player.pause();
+            //player.pause();
+            player.stop();
 
         try {
             player = new MediaPlayer(music);
@@ -383,65 +198,63 @@ public class MusicPlayerController implements Initializable {
             alert.showAndWait();
             return;
         }
-        player.setVolume(volumeSlider.getValue());
-        player.setOnReady(new Runnable() {
-            @Override
-            public void run() {
-                player.setStartTime(Duration.ZERO);
-                img.setMediaPlayer(player);
 
-                if (player.getMedia().getSource().contains(".mp3") || player.getMedia().getSource().contains(".wav") ||
-                        player.getMedia().getSource().contains(".aif") || player.getMedia().getSource().contains(".aiff") ||
-                        player.getMedia().getSource().contains(".m4a")
-                ) {
-                    imgView.toFront();
-                } else {
-                    img.setMediaPlayer(player);
-                    img.toFront();
+        playButton.setIconName("PAUSE");
+        if (voiceFlag) {
+            player.setVolume(0.25);
+            voiceFlag = false;
+        } else {
+            player.setVolume(volumeSlider.getValue());
+        }
+        player.setOnReady(() -> {
+            player.setStartTime(Duration.ZERO);
+
+            if (player.getMedia().getSource().contains(".mp3") || player.getMedia().getSource().contains(".wav")) {
+                imgView.toFront();
+            } else {
+                img.setMediaPlayer(player);
+                img.toFront();
+            }
+
+            //VOLUME PROPERTİES
+            volumeSlider.setMin(0);
+            volumeSlider.setMax(0.5);
+            volumeSlider.setValue(player.getVolume());
+            VolumeText.setText(String.valueOf((int) (volumeSlider.getValue() * 100 * 2)));
+
+            volumeSlider.valueProperty().addListener(evs -> {
+                player.setVolume(volumeSlider.getValue());
+                VolumeText.setText(String.valueOf((int) (volumeSlider.getValue() * 100 * 2)));
+
+            });
+
+            playingTimeSlider.setMax(player.getStopTime().toMillis());
+            player.currentTimeProperty().addListener(ev -> {
+                remainingTime.setText(String.valueOf((int) (player.getStopTime().toSeconds() - player.getCurrentTime().toSeconds())));
+                playingTimeSlider.valueProperty().setValue(player.getCurrentTime().toMillis());
+                if (!playingTimeSlider.isValueChanging()) {
+                    playingTimeSlider.setValue(player.getCurrentTime().toMillis());
+                }
+            });
+
+            player.play();
+            playedSongs.push(player.getMedia().getSource().substring(player.getMedia().getSource().lastIndexOf("/") + 1));
+            //System.out.println(playedSongs.peek());
+            //StaticController.Amblem.setFill(Color.rgb(172,60,60,1));
+            playingTimeSlider.valueProperty().addListener(ov -> {
+                if (playingTimeSlider.isValueChanging()) {
+                    player.seek(Duration.millis(new Duration(playingTimeSlider.getValue()).toMillis()));
                 }
 
+            });
 
-                //VOLUME PROPERTİES
-                volumeSlider.setMin(0);
-                volumeSlider.setMax(0.5);
-
-                VolumeText.setText(String.valueOf((int) (player.getVolume() * 100 * 2)));
-
-                volumeSlider.valueProperty().addListener(evs -> {
-                    player.setVolume(volumeSlider.getValue());
-                    VolumeText.setText(String.valueOf((int) (volumeSlider.getValue() * 100 * 2)));
-
-                });
-
-                playingTimeSlider.setMax(player.getStopTime().toMillis());
-                player.currentTimeProperty().addListener(ev -> {
-                    remainingTime.setText(String.valueOf((int) (player.getStopTime().toSeconds() - player.getCurrentTime().toSeconds())));
-                    playingTimeSlider.valueProperty().setValue(player.getCurrentTime().toMillis());
-                    if (!playingTimeSlider.isValueChanging()) {
-                        playingTimeSlider.setValue(player.getCurrentTime().toMillis());
-                    }
-                });
-
-                playingTimeSlider.valueProperty().addListener(ov -> {
-                    if (playingTimeSlider.isValueChanging()) {
-                        player.seek(Duration.millis(new Duration(playingTimeSlider.getValue()).toMillis()));
-                    }
-
-                });
-                player.play();
-                playedSongs.push(player.getMedia().getSource().substring(player.getMedia().getSource().lastIndexOf("/") + 1));
-
-                player.setOnEndOfMedia(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            playRandomMusic();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
+            player.setOnEndOfMedia(() -> {
+                try {
+                    playRandomMusic();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         });
 
     }
